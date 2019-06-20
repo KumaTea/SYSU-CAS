b3f171a5f98b5263281442ba3d8617f43fddd53a
KumaTea <36222458+KumaTea@users.noreply.github.com>
2019-06-21 05:58:40 +0800

    using class

--- login.py
+++ login.py
@@ -6,53 +6,60 @@ from PIL import Image
 from bs4 import BeautifulSoup
 from getpass import getpass
 
-cas_url = 'https://cas.sysu.edu.cn/cas/login'
-captcha_url = 'https://cas.sysu.edu.cn/cas/captcha.jsp'
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
-    'Host': 'cas.sysu.edu.cn',
-    'Referer': 'https://cas.sysu.edu.cn/',
-}
-
-
-def get_xsrf():
-    resp = sysu_cas.get(cas_url, headers=headers, cookies=sysu_cas.cookies.get_dict(), verify=False)
-    soup = BeautifulSoup(resp.content, "html.parser")
-    xsrf = soup.find('input', attrs={'name': 'execution'}).get("value")
-    return xsrf
-
-
-def get_captcha():
-    get_capt = sysu_cas.get(captcha_url, headers=headers, cookies=sysu_cas.cookies.get_dict())
-    if not os.path.exists('temp'):
-        os.mkdir('temp')
-    with open('temp/captcha.jpg', 'wb') as ca:
-        ca.write(get_capt.content)
-    with Image.open('temp/captcha.jpg') as captcha_image:
-        captcha_image.show()
-    captcha_code = input('Please input the code in this image:\n')
-    os.remove('temp/captcha.jpg')
-    return captcha_code
-
-
-def login(username, password):
-    login_data = {
-        'username': username,
-        'password': password,
-        'captcha': get_captcha(),
-        'execution': get_xsrf(),
-        '_eventId': 'submit',
-        'geolocation': '',
+class CasLogin:
+    cas_url = 'https://cas.sysu.edu.cn/cas/login'
+    captcha_url = 'https://cas.sysu.edu.cn/cas/captcha.jsp'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
+        'Host': 'cas.sysu.edu.cn',
+        'Referer': 'https://cas.sysu.edu.cn/',
     }
-    result = sysu_cas.post(cas_url, headers=headers, cookies=sysu_cas.cookies.get_dict(), data=login_data)
-    # print(sysu_cas.cookies.get_dict())
-    if 'success' in result.text:
-        return sysu_cas.cookies.get_dict()
-    elif 'credential' in result.text:
-        return 'password'
-    else:
-        return 'captcha'
+
+    def __init__(self):
+        self.session = requests.Session()
+
+    def get_xsrf(self):
+        resp = self.session.get(self.cas_url, headers=self.headers, cookies=self.session.cookies.get_dict(), verify=False)
+        soup = BeautifulSoup(resp.content, "html.parser")
+        xsrf = soup.find('input', attrs={'name': 'execution'}).get("value")
+        return xsrf
+
+    def get_captcha(self):
+        get_capt = self.session.get(self.captcha_url, headers=self.headers, cookies=self.session.cookies.get_dict())
+        if not os.path.exists('temp'):
+            os.mkdir('temp')
+        with open('temp/captcha.jpg', 'wb') as ca:
+            ca.write(get_capt.content)
+        with Image.open('temp/captcha.jpg') as captcha_image:
+            captcha_image.show()
+        captcha_code = input('Please input the code in this image:\n')
+        os.remove('temp/captcha.jpg')
+        return captcha_code
+
+    def login_cas(self, username, password):
+        login_data = {
+            'username': username,
+            'password': password,
+            'captcha': self.get_captcha(),
+            'execution': self.get_xsrf(),
+            '_eventId': 'submit',
+            'geolocation': '',
+        }
+        result = self.session.post(self.cas_url, headers=self.headers, cookies=self.session.cookies.get_dict(), data=login_data)
+        # print(self.session.cookies.get_dict())
+        if 'success' in result.text:
+            return self.session.cookies.get_dict()
+        elif 'credential' in result.text:
+            return 'password'
+        else:
+            return 'captcha'
+
+    def login(self):
+        user_name = input('Please input your NetID:\n')
+        user_pass = getpass('Please input your password:\n')
+        login_result = self.login_cas(user_name, user_pass)
+        return login_result
 
 
 def test_status(result):
@@ -63,12 +70,3 @@ def test_status(result):
             print('Maybe captcha is wrong.')
         else:
             print('Maybe password does not match.')
-
-
-if __name__ == '__main__':
-    sysu_cas = requests.Session()
-    user_name = input('Please input your NetID:\n')
-    user_pass = getpass('Please input your password:\n')
-    login_result = login(user_name, user_pass)
-    test_status(login_result)
-    sysu_cas.close()
